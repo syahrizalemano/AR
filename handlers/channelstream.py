11c4211cd22258be3414f59b96d3cc5a1c3125fe
syahrizalemano <89107294+syahrizalemano@users.noreply.github.com>
2021-09-14 17:15:31 +0800

    Update channelstream.py

--- handlers/channelstream.py
+++ handlers/channelstream.py
@@ -447,7 +447,9 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton("⏺ Menu", callback_data="cmenu"),
                     InlineKeyboardButton("🗑 Close", callback_data="ccls")
-            ]
+            
+                ]
+            ]    
         )
         file_name = get_file_name(audio)
         title = file_name
